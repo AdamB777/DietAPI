931bf5d19de3afd63f92d4a7927595e6255b232e
AdamB777 <akrupa777@gmail.com>
2023-09-06 11:18:41 +0200

    asd

--- ModelsDB/Functionality/Address.cs
+++ ModelsDB/Functionality/Address.cs
@@ -15,11 +15,9 @@ namespace ModelsDB
         public string Street { get; set; }
         public string LocalNo { get; set; }
 
-        public List<User> Users { get; set; }
+        public List<MyUser> Users { get; set; }
         public int OfficeId { get; set; }
         public Office Office { get; set; }
-        public List<Dietician>Dieticians { get; set; }
-        public List<Patient> Patient { get; set; }
 
     }
 }
